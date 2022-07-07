edd52031e9f462ef71e8ba917eb2818ec7ee477c
Adeleke Oluwafemi <62451630+adeleke-code@users.noreply.github.com>
2022-07-07 13:18:58 +0100

    fix

--- myblog/settings.py
+++ myblog/settings.py
@@ -99,6 +99,7 @@ if environment == "Development":
 
 elif environment == "Staging":
     DATABASES = {}
+    DATABASES['ENGINE'] = 'django.db.backends.postgresql_psycopg2',
     DATABASES['default'] = dj_database_url.config()
 
 
