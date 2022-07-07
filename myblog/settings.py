c26ef7e62a7f1ff7ed3117fd50c6c5b264d6082a
Adeleke Oluwafemi <62451630+adeleke-code@users.noreply.github.com>
2022-07-07 11:53:27 +0100

    deployed

--- myblog/settings.py
+++ myblog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("MYKEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["femi-blog.herokuapp.com"]
 
 
 # Application definition
